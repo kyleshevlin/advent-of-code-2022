19dee724903c698d2e66e0cda7a1c460b010a73d
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2022-12-02 09:05:04 -0800

    add info to day 1

--- src/day-01/index.js
+++ src/day-01/index.js
@@ -1,10 +1,16 @@
-const { getData, sum } = require('../utils')
+/**
+ * Day 1 is a sorting problem. The elves are carrying food items of various
+ * calorie counts. We need to sum the calories each elf, sort them from
+ * greatest to least.
+ *
+ * The first solution asked for only the greatest calorie sum, and so Math.max
+ * essentially sorts for us.
+ *
+ * The second asked for the top three, so sorting the sums and then summing a
+ * slice of that was the trick.
+ */
 
-module.exports = {
-  getElves,
-  getGreatestCalorieSum,
-  getGreatestCalorieSumOfTopThree,
-}
+const { getData, sum } = require('../utils')
 
 const data = getData(__dirname)
 
@@ -29,5 +35,12 @@ function getGreatestCalorieSumOfTopThree(input) {
 
 const firstAnswer = getGreatestCalorieSum(data)
 // console.log(firstAnswer) // 68467
+
 const secondAnswer = getGreatestCalorieSumOfTopThree(data)
 // console.log(secondAnswer) // 203420
+
+module.exports = {
+  getElves,
+  getGreatestCalorieSum,
+  getGreatestCalorieSumOfTopThree,
+}
