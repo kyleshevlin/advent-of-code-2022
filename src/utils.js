ddbecf56a383ec6d96494e02d1ffd7fe9465cc9c
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2022-12-01 21:40:03 -0800

    add comment about trace for anyone that might read the code

--- src/utils.js
+++ src/utils.js
@@ -11,6 +11,17 @@ const add = (x, y) => x + y
 
 const sum = nums => nums.reduce(add, 0)
 
+/**
+ * Use this with map to log out the current value next to the message while
+ * still returning the value to keep your composition working. Example:
+ *
+ * ```javascript
+ * const result = [1, 2, 3]
+ *   .map(double)
+ *   .map(trace('after double'))
+ *   .reduce(add, 0)
+ * ```
+ */
 const trace = msg => x => (console.log(msg, x), x)
 
 module.exports = {
