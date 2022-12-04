fefc8adafa16953c4645998ac95dfafa27901265
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2022-12-03 20:20:05 -0800

    update all days to use solution1 and solution2 pattern to save time

--- src/day-07/index.test.js
+++ src/day-07/index.test.js
@@ -1,1 +1,11 @@
-test.todo('Add test')
+const { solution1, solution2 } = require('./')
+
+const input = ``
+
+test('solution1', () => {
+  expect(solution1(input)).toEqual(undefined)
+})
+
+test('solution2', () => {
+  expect(solution2(input)).toEqual(undefined)
+})
--- src/day-16/index.js
+++ src/day-16/index.js
@@ -1,3 +1,18 @@
 const { getData } = require('../utils')
 
 const data = getData(__dirname)
+
+function solution1(input) {}
+
+const firstAnswer = solution1(data)
+// console.log(firstAnswer)
+
+function solution2(input) {}
+
+const secondAnswer = solution2(data)
+// console.log(secondAnswer)
+
+module.exports = {
+  solution1,
+  solution2,
+}
