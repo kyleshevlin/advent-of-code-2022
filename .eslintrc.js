8b7cbdbacc640fbebcc415ffd0fbd8467cd24118
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2022-12-03 20:28:06 -0800

    add linting

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+    'jest/globals': true,
+  },
+  extends: ['eslint:recommended', 'prettier'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  plugins: ['jest'],
+  rules: {
+    'no-unused-vars': 'warn',
+  },
+}
