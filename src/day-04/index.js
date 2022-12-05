afaf3f2da8822be603efce290b91b5cfaa331ccd
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2022-12-04 16:36:42 -0800

    add comment about day 4

--- src/day-04/index.js
+++ src/day-04/index.js
@@ -1,3 +1,17 @@
+/**
+ * Day 4 involved elves having to clean up sections of their camp.
+ * To do this, each pair of elves is assigned a set of consecutive sections.
+ *
+ * First problem was to determine if one elf is cleaning section entirely
+ * contained by the other elf. This can be solved, but comparing the `from`s of
+ * both elves and the `to`s of each elf's range.
+ *
+ * The second problem was to determine if there was any overlap at all. This
+ * could be solved with set intersection (again). We create a range of _all_ the
+ * sections an elf had to clean, and if there are any sections in the
+ * intersection of those two sets, then we know we have overlap.
+ */
+
 const { getData, intersection, createRange } = require('../utils')
 
 const data = getData(__dirname)
