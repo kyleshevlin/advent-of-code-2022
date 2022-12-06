300af56510cc8ecf0bb502e2a9832eaaaad4ffe0
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2022-12-05 21:18:51 -0800

    add solution for day 6

--- src/day-06/index.js
+++ src/day-06/index.js
@@ -1,16 +1,44 @@
+/**
+ * Day 6 involves parsing a substring for unique characters. This is really simple
+ * with the use of Sets.
+ *
+ * Sets are a collection of unique items. You'll never have a duplicate item in
+ * a set. So substrings with duplicated characters will have a smaller size than a
+ * substring that has all unique characters.
+ *
+ * Thus, we check that a set is the size of the substring length want and we
+ * break our loop at that index.
+ */
+
 const { getData } = require('../utils')
 
 const data = getData(__dirname)
 
-function solution1(input) {}
+function indexOfFirstSetAllUniqueCharacters(input, size) {
+  let i
+  for (i = size; i < input.length - 1; i++) {
+    const chars = input.substring(i - size, i).split('')
+    const set = new Set(chars)
+
+    if (set.size === size) break
+  }
+
+  return i
+}
+
+function solution1(input) {
+  return indexOfFirstSetAllUniqueCharacters(input, 4)
+}
 
 const firstAnswer = solution1(data)
-// console.log(firstAnswer)
+// console.log(firstAnswer) // 1702
 
-function solution2(input) {}
+function solution2(input) {
+  return indexOfFirstSetAllUniqueCharacters(input, 14)
+}
 
 const secondAnswer = solution2(data)
-// console.log(secondAnswer)
+// console.log(secondAnswer) // 3559
 
 module.exports = {
   solution1,
--- src/day-06/index.test.js
+++ src/day-06/index.test.js
@@ -1,11 +1,23 @@
 const { solution1, solution2 } = require('./')
 
-const input = ``
+const input1 = `mjqjpqmgbljsphdztnvjfqwrcgsmlb`
+const input2 = `bvwbjplbgvbhsrlpgdmjqwftvncz`
+const input3 = `nppdvjthqldpwncqszvftbrmjlhg`
+const input4 = `nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg`
+const input5 = `zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw`
 
 test('solution1', () => {
-  expect(solution1(input)).toEqual(undefined)
+  expect(solution1(input1)).toEqual(7)
+  expect(solution1(input2)).toEqual(5)
+  expect(solution1(input3)).toEqual(6)
+  expect(solution1(input4)).toEqual(10)
+  expect(solution1(input5)).toEqual(11)
 })
 
 test('solution2', () => {
-  expect(solution2(input)).toEqual(undefined)
+  expect(solution2(input1)).toEqual(19)
+  expect(solution2(input2)).toEqual(23)
+  expect(solution2(input3)).toEqual(23)
+  expect(solution2(input4)).toEqual(29)
+  expect(solution2(input5)).toEqual(26)
 })
