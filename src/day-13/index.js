eeea0809af141cc666cfbf357201a650aeb2aa68
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2022-12-12 22:17:16 -0800

    adds solution for day 13

--- src/day-13/index.js
+++ src/day-13/index.js
@@ -1,16 +1,104 @@
-const { getData } = require('../utils')
+const { getData, sum } = require('../utils')
 
 const data = getData(__dirname)
 
-function solution1(input) {}
+function parseInput(input) {
+  return input
+    .trim()
+    .split('\n\n')
+    .map(pairs => pairs.split('\n').map(JSON.parse))
+}
 
-const firstAnswer = solution1(data)
-// console.log(firstAnswer)
+function compareLists(left, right) {
+  const tmpLeft = [...left]
+  const tmpRight = [...right]
+  let result
 
-function solution2(input) {}
+  while (tmpLeft.length && tmpRight.length) {
+    let leftItem = tmpLeft.shift()
+    let rightItem = tmpRight.shift()
 
-const secondAnswer = solution2(data)
-// console.log(secondAnswer)
+    if (typeof leftItem === 'number' && typeof rightItem === 'number') {
+      if (leftItem < rightItem) {
+        result = 'correct'
+        break
+      }
+
+      if (leftItem > rightItem) {
+        result = 'wrong'
+        break
+      }
+
+      if (leftItem === rightItem) {
+        continue
+      }
+    }
+
+    if (!Array.isArray(leftItem)) {
+      leftItem = [leftItem]
+    }
+
+    if (!Array.isArray(rightItem)) {
+      rightItem = [rightItem]
+    }
+
+    result = compareLists(leftItem, rightItem)
+
+    if (result === 'correct') break
+    if (result === 'wrong') break
+  }
+
+  if (result) return result
+
+  if (tmpLeft.length && !tmpRight.length) result = 'wrong'
+  if (tmpRight.length && !tmpLeft.length) result = 'correct'
+
+  return result
+}
+
+function solution1(input) {
+  const pairs = parseInput(input)
+  const indices = []
+
+  for (const [idx, [left, right]] of pairs.entries()) {
+    const result = compareLists(left, right)
+
+    if (result === 'correct') {
+      indices.push(idx + 1)
+    }
+  }
+
+  return sum(indices)
+}
+
+// const firstAnswer = solution1(data)
+// console.log(firstAnswer) // 6478
+
+function secondParseInput(input) {
+  return input.trim().split('\n').filter(Boolean).map(JSON.parse)
+}
+
+function solution2(input) {
+  const lines = secondParseInput(input)
+  const div1 = [[2]]
+  const div2 = [[6]]
+  const allLines = [...lines, div1, div2]
+  const sorted = allLines.sort((a, b) => {
+    const result = compareLists(a, b)
+
+    if (result === 'correct') return -1
+    if (result === 'wrong') return 1
+    return 0
+  })
+
+  const div1Index = sorted.findIndex(x => x === div1) + 1
+  const div2Index = sorted.findIndex(x => x === div2) + 1
+
+  return div1Index * div2Index
+}
+
+// const secondAnswer = solution2(data)
+// console.log(secondAnswer) // 21922
 
 module.exports = {
   solution1,
--- src/day-13/index.test.js
+++ src/day-13/index.test.js
@@ -1,11 +1,35 @@
 const { solution1, solution2 } = require('./')
 
-const input = ``
+const input = `
+[1,1,3,1,1]
+[1,1,5,1,1]
+
+[[1],[2,3,4]]
+[[1],4]
+
+[9]
+[[8,7,6]]
+
+[[4,4],4,4]
+[[4,4],4,4,4]
+
+[7,7,7,7]
+[7,7,7]
+
+[]
+[3]
+
+[[[]]]
+[[]]
+
+[1,[2,[3,[4,[5,6,7]]]],8,9]
+[1,[2,[3,[4,[5,6,0]]]],8,9]
+`
 
 test('solution1', () => {
-  expect(solution1(input)).toEqual(undefined)
+  expect(solution1(input)).toEqual(13)
 })
 
 test('solution2', () => {
-  expect(solution2(input)).toEqual(undefined)
+  expect(solution2(input)).toEqual(140)
 })
