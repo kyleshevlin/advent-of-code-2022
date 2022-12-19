5d462fa44fce08430a6421c9f0ed142ac0731e7e
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2022-12-18 16:29:23 -0800

    solution for day 16 pt 1

--- src/day-16/index.js
+++ src/day-16/index.js
@@ -2,14 +2,140 @@ const { getData } = require('../utils')
 
 const data = getData(__dirname)
 
-function solution1(input) {}
+function createValve(key, flow, connections) {
+  return {
+    key,
+    flow,
+    connections,
+  }
+}
 
-const firstAnswer = solution1(data)
-// console.log(firstAnswer)
+function parseInput(input) {
+  return input
+    .trim()
+    .split('\n')
+    .map(line => {
+      const key = line.substring(6, 8)
+      const [flow] = line.match(/(?<=rate=)\d*/).map(Number)
+      const connections = line
+        .split(/to valves? /)[1]
+        .split(',')
+        .map(x => x.trim())
+
+      return createValve(key, flow, connections)
+    })
+}
+
+function getValvesMap(valves) {
+  const result = {}
+
+  for (const valve of valves) {
+    result[valve.key] = valve
+  }
+
+  return result
+}
+
+function createDistanceMap(valves) {
+  const valvesMap = getValvesMap(valves)
+
+  const result = {}
+
+  for (const valve of valves) {
+    const _result = {}
+    const visited = {}
+
+    // eslint-disable-next-line
+    function traverse(valve, visitFn, depth = 0) {
+      if (visited[valve.key] && _result[valve.key] < depth) return
+
+      visitFn(valve, depth)
+      visited[valve.key] = true
+
+      for (const key of valve.connections) {
+        const connection = valvesMap[key]
+        traverse(connection, visitFn, depth + 1)
+      }
+    }
+
+    traverse(valve, ({ key }, depth) => {
+      _result[key] = depth
+    })
+
+    result[valve.key] = _result
+  }
+
+  return result
+}
+
+function maximizePressureRelease(valves, minutes) {
+  const distanceMap = createDistanceMap(valves)
+  const start = valves.find(v => v.key == 'AA')
+  const withFlow = valves.filter(v => v.flow > 0)
+  // console.log([...withFlow].sort((a, b) => b.flow - a.flow))
+
+  let result = -Infinity
+  let winningPath
+  function traverse(
+    node,
+    path,
+    nodesRemaining,
+    minutesRemaining,
+    accumulatedPressure
+  ) {
+    const viableNodes = nodesRemaining.filter(remaining => {
+      const travel = distanceMap[node.key][remaining.key]
+      const cost = travel + 1
+
+      return cost < minutesRemaining
+    })
+
+    if (!viableNodes.length && accumulatedPressure > result) {
+      result = accumulatedPressure
+      winningPath = path
+      return
+    }
+
+    for (const remaining of viableNodes) {
+      const minutesToTravel = distanceMap[node.key][remaining.key]
+      // Takes an additional minute to open the valve
+      const minutesCost = minutesToTravel + 1
+      const nextMinutesRemaining = minutesRemaining - minutesCost
+
+      const pressure = nextMinutesRemaining * remaining.flow
+      const nextNodesRemaining = viableNodes.filter(
+        n => n.key !== remaining.key
+      )
+      const nextAccumulatedPressure = accumulatedPressure + pressure
+
+      traverse(
+        remaining,
+        path + `->${remaining.key}(${pressure})`,
+        nextNodesRemaining,
+        nextMinutesRemaining,
+        nextAccumulatedPressure
+      )
+    }
+  }
+
+  traverse(start, 'AA(0)', withFlow, minutes, 0)
+
+  return result
+}
+
+function solution1(input) {
+  const valves = parseInput(input)
+  const result = maximizePressureRelease(valves, 30)
+
+  return result
+}
+
+// const firstAnswer = solution1(data)
+// console.log(firstAnswer) // 1460
 
 function solution2(input) {}
 
-const secondAnswer = solution2(data)
+// const secondAnswer = solution2(data)
 // console.log(secondAnswer)
 
 module.exports = {
--- src/day-16/index.test.js
+++ src/day-16/index.test.js
@@ -1,9 +1,20 @@
 const { solution1, solution2 } = require('./')
 
-const input = ``
+const input = `
+Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
+Valve BB has flow rate=13; tunnels lead to valves CC, AA
+Valve CC has flow rate=2; tunnels lead to valves DD, BB
+Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE
+Valve EE has flow rate=3; tunnels lead to valves FF, DD
+Valve FF has flow rate=0; tunnels lead to valves EE, GG
+Valve GG has flow rate=0; tunnels lead to valves FF, HH
+Valve HH has flow rate=22; tunnel leads to valve GG
+Valve II has flow rate=0; tunnels lead to valves AA, JJ
+Valve JJ has flow rate=21; tunnel leads to valve II
+`
 
 test('solution1', () => {
-  expect(solution1(input)).toEqual(undefined)
+  expect(solution1(input)).toEqual(1651)
 })
 
 test('solution2', () => {
