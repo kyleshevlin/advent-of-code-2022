43df7ab7ef6e6f3748c1617850f131894119fd35
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2022-12-22 08:42:46 -0800

    solution for day 18 pt 1

--- src/day-18/index.js
+++ src/day-18/index.js
@@ -1,11 +1,45 @@
-const { getData } = require('../utils')
+const { getData, intersection } = require('../utils')
 
 const data = getData(__dirname)
 
-function solution1(input) {}
+function parseInput(input) {
+  return input.trim().split('\n')
+}
 
-const firstAnswer = solution1(data)
-// console.log(firstAnswer)
+const stringToTuple = str => str.split(',').map(Number)
+const tupleToString = tup => tup.join(',')
+
+const getNeighborKeys = cubeStr => {
+  const [x, y, z] = stringToTuple(cubeStr)
+
+  return [
+    [x + 1, y, z],
+    [x - 1, y, z],
+    [x, y + 1, z],
+    [x, y - 1, z],
+    [x, y, z + 1],
+    [x, y, z - 1],
+  ].map(tupleToString)
+}
+
+function solution1(input) {
+  const cubes = parseInput(input)
+  const cubeSet = new Set(cubes)
+
+  // Let's solve this naively for now
+  let sides = 0
+  for (const cube of cubes) {
+    const neighbors = new Set(getNeighborKeys(cube))
+    const intersected = intersection(cubeSet, neighbors)
+
+    sides += 6 - intersected.size
+  }
+
+  return sides
+}
+
+// const firstAnswer = solution1(data)
+// console.log(firstAnswer) // 3662
 
 function solution2(input) {}
 
--- src/day-18/index.test.js
+++ src/day-18/index.test.js
@@ -1,9 +1,23 @@
 const { solution1, solution2 } = require('./')
 
-const input = ``
+const input = `
+2,2,2
+1,2,2
+3,2,2
+2,1,2
+2,3,2
+2,2,1
+2,2,3
+2,2,4
+2,2,6
+1,2,5
+3,2,5
+2,1,5
+2,3,5
+`
 
 test('solution1', () => {
-  expect(solution1(input)).toEqual(undefined)
+  expect(solution1(input)).toEqual(64)
 })
 
 test('solution2', () => {
