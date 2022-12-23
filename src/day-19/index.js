60e9b8b44307228c36957f42d4ec4f31faaab532
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2022-12-23 09:28:12 -0800

    parse input day 19

--- src/day-19/index.js
+++ src/day-19/index.js
@@ -2,14 +2,59 @@ const { getData } = require('../utils')
 
 const data = getData(__dirname)
 
-function solution1(input) {}
+function parseInput(input) {
+  return input
+    .trim()
+    .split('\n')
+    .map(str => {
+      const [id] = str.match(/(?<=Blueprint )\d+/).map(Number)
+      const [oreRobotCost] = str
+        .match(/(?<=Each ore robot costs )[0-9]+/)
+        .map(Number)
+      const [clayRobotCost] = str
+        .match(/(?<=Each clay robot costs )[0-9]+/)
+        .map(Number)
+      const obsidianRobotCosts = str.match(
+        /(?<=Each obsidian robot costs )(?<ore>[0-9]+) ore and (?<clay>[0-9]+)/
+      )
+      const geodeRobotCosts = str.match(
+        /(?<=Each geode robot costs )(?<ore>[0-9]+) ore and (?<obsidian>[0-9]+)/
+      )
 
-const firstAnswer = solution1(data)
+      return {
+        id,
+        oreRobot: {
+          cost: { ore: oreRobotCost },
+        },
+        clayRobot: {
+          cost: { ore: clayRobotCost },
+        },
+        obsidianRobot: {
+          cost: {
+            ore: Number(obsidianRobotCosts.groups.ore),
+            clay: Number(obsidianRobotCosts.groups.clay),
+          },
+        },
+        geodeRobot: {
+          cost: {
+            ore: Number(geodeRobotCosts.groups.ore),
+            obsidian: Number(geodeRobotCosts.groups.obsidian),
+          },
+        },
+      }
+    })
+}
+
+function solution1(input) {
+  return parseInput(input)
+}
+
+// const firstAnswer = solution1(data)
 // console.log(firstAnswer)
 
 function solution2(input) {}
 
-const secondAnswer = solution2(data)
+// const secondAnswer = solution2(data)
 // console.log(secondAnswer)
 
 module.exports = {
--- src/day-19/index.test.js
+++ src/day-19/index.test.js
@@ -1,9 +1,12 @@
 const { solution1, solution2 } = require('./')
 
-const input = ``
+const input = `
+Blueprint 1: Each ore robot costs 4 ore. Each clay robot costs 2 ore. Each obsidian robot costs 3 ore and 14 clay. Each geode robot costs 2 ore and 7 obsidian.
+Blueprint 2: Each ore robot costs 2 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 8 clay. Each geode robot costs 3 ore and 12 obsidian.
+`
 
 test('solution1', () => {
-  expect(solution1(input)).toEqual(undefined)
+  expect(solution1(input)).toEqual(33)
 })
 
 test('solution2', () => {
