c23b06510d0e95898d9b405ce70b02619ad656f2
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2022-12-24 11:27:11 -0800

    solution for day 21 pt 1

--- src/day-21/index.js
+++ src/day-21/index.js
@@ -1,15 +1,75 @@
-const { getData } = require('../utils')
+const { getData, add, subtract, multiply, divide } = require('../utils')
 
 const data = getData(__dirname)
 
-function solution1(input) {}
+const OPS = ['+', '-', '*', '/']
 
-const firstAnswer = solution1(data)
-// console.log(firstAnswer)
+const OP_TO_FN = {
+  '+': add,
+  '-': subtract,
+  '*': multiply,
+  '/': divide,
+}
+
+function parseInput(input) {
+  return input
+    .trim()
+    .split('\n')
+    .map(line => {
+      const [key, value] = line.split(':').map(x => x.trim())
+
+      let formattedValue
+      if (OPS.some(op => value.includes(op))) {
+        formattedValue = value.split(' ')
+      } else {
+        formattedValue = Number(value)
+      }
+
+      return { [key]: formattedValue }
+    })
+    .reduce((acc, cur) => {
+      return { ...acc, ...cur }
+    }, {})
+}
+
+function solution1(input) {
+  const monkeys = parseInput(input)
+  const solved = Object.fromEntries(
+    Object.entries(monkeys).filter(([_, value]) => {
+      return typeof value === 'number'
+    })
+  )
+
+  let dirty = true
+  while (dirty) {
+    dirty = false
+
+    for (const [key, value] of Object.entries(monkeys)) {
+      if (solved[key] !== undefined) continue
+
+      const [x, op, y] = value
+
+      const xVal = solved[x]
+      const yVal = solved[y]
+
+      if (xVal !== undefined && yVal !== undefined) {
+        const result = OP_TO_FN[op](xVal, yVal)
+        solved[key] = result
+      }
+
+      dirty = true
+    }
+  }
+
+  return solved.root
+}
+
+// const firstAnswer = solution1(data)
+// console.log(firstAnswer) // 160274622817992
 
 function solution2(input) {}
 
-const secondAnswer = solution2(data)
+// const secondAnswer = solution2(data)
 // console.log(secondAnswer)
 
 module.exports = {
--- src/day-21/index.test.js
+++ src/day-21/index.test.js
@@ -1,9 +1,25 @@
 const { solution1, solution2 } = require('./')
 
-const input = ``
+const input = `
+root: pppw + sjmn
+dbpl: 5
+cczh: sllz + lgvd
+zczc: 2
+ptdq: humn - dvpt
+dvpt: 3
+lfqf: 4
+humn: 5
+ljgn: 2
+sjmn: drzm * dbpl
+sllz: 4
+pppw: cczh / lfqf
+lgvd: ljgn * ptdq
+drzm: hmdt - zczc
+hmdt: 32
+`
 
 test('solution1', () => {
-  expect(solution1(input)).toEqual(undefined)
+  expect(solution1(input)).toEqual(152)
 })
 
 test('solution2', () => {
